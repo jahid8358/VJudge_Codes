dac020ced90f932f8315680d1707f7955bf15e50
jahid8358 <jahidhasan8358@gmail.com>
2026-09-13 12:30:05 +0600

    Solved: problem (vjudge)

---
+++ vjudge/Watermelon---CodeForces-4A---Virtual-Judge/67858160.cpp
@@ -0,0 +1,21 @@
+// Problem: problem
+// Platform: vjudge
+// Contest: Watermelon - CodeForces 4A - Virtual Judge
+// Language: C++17 (GCC 7-32)
+// Verdict: Accepted
+// URL: https://vjudge.net/problem/CodeForces-4A
+// Solved on: 2026-09-13T06:30:05.007Z
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+if(n>2 && n%2==0)
+    cout<<"YES"<<endl;
+else
+    cout<<"NO"<<endl;
+    return 0;
+}
